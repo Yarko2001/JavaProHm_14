a218a2db0abdcb8cc91b54db4dd7e3e290120fe8
Degtiar Yaroslav <merrynan2001@gmail.com>
2022-11-01 23:32:15 +0300

    fixed mistake in main method

--- src/com/company/hillel/dehtiar/MyFacebook.java
+++ src/com/company/hillel/dehtiar/MyFacebook.java
@@ -9,26 +9,26 @@ import com.company.hillel.dehtiar.entity.users.Writer;
 
 public class MyFacebook {
 
-  public static <Picture> void main(String[] args) {
+  public static void main(String[] args) {
     Writer tomasShelbi = new Writer("@TomShelbi");
     Writer maxTruhovskiy = new Writer("@max_true");
 
-    tomasShelbi.follow(new Follower("@Sensei",21));
-    tomasShelbi.follow(new Follower("@TrepleX",33));
-    tomasShelbi.follow(new BestFriend("@Helix",24));
+    tomasShelbi.follow(new Follower("@Sensei", 21));
+    tomasShelbi.follow(new Follower("@TrepleX", 33));
+    tomasShelbi.follow(new BestFriend("@Helix", 24));
 
     tomasShelbi.post("It was an amazing week !!!");
 
-    Post video = new Video("Sunshine",1024L);
-    Post photo = new Photo("Apple",230L);
+    Post video = new Video("Sunshine", 1024L);
+    Post photo = new Photo("Apple", 230L);
     tomasShelbi.load(video);
     tomasShelbi.load(photo);
     tomasShelbi.post(video);
     tomasShelbi.post(photo);
 
-    maxTruhovskiy.follow(new Follower("@Drax",43));
+    maxTruhovskiy.follow(new Follower("@Drax", 43));
     maxTruhovskiy.follow(tomasShelbi);
-    maxTruhovskiy.post("Hey,how are doing there?",tomasShelbi);
+    maxTruhovskiy.post("Hey,how are doing there?", tomasShelbi);
 
   }
 }
